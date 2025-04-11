557f5312e7f9d6b9cbcd516b4c5530cca120c0ec
unknown <furtadocasagrandepablo@gmail.com>
2025-04-11 11:15:34 -0300

    :tada: Commit Inicial

---
+++ helloworld/src/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello PetWorld!!!\n");
+        System.out.println("A turma JAVA82 é TOP!!!");
+	}
+
+}
